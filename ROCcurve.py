047f11725312c6c72f9cce0e74a8ab287c63011e
Shiyu Wang <shiyuwang@Jennifers-MacBook-Pro-3.local>
2019-04-16 11:15:21 -0400

    init commit

---
+++ ROCcurve.py
@@ -0,0 +1,11 @@
+'''
+This file is used to calculate TP/FP used to draw the ROC curve
+argument list:
+arg[0]: name of the script
+arg[1]: Lack of Motion file name -- .txt expected
+arg[2]: BodiTrak mat file name -- .xlsx expected
+arg[3]: threshold for Lack of Motion file
+arg[4]: threshold for BodiTrak mat
+'''
+import sys
+
